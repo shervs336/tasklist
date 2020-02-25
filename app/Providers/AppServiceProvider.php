a249ff288da8dd6a59e724b3caf9b1e091f86504
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2020-02-25 19:00:25 +0800

    Define components call

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::component('components.content-header', 'contentHeader');
+        Blade::component('components.breadcrumb', 'breadcrumb');
+        Blade::component('components.alert', 'alert');
+        Blade::component('components.info-box', 'infoBox');
     }
 }
